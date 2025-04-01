d5b6a443ed8465d443ec69c285ad8ad6f53687e1
unknown <ronyedson@yahoo.com.br>
2025-04-01 08:45:16 -0300

    primeira subida 2

---
+++ src/app/app.component.ts
@@ -0,0 +1,60 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  @ViewChild(MatSidenav, {static: true})
+  sidenav!: MatSidenav;
+  vertebrates: any;
+  submenu:  boolean = true;
+
+  @ViewChild('sidenav1') sidenav1!: MatSidenav;
+  isExpanded = true;
+  showSubmenu: boolean = false;
+  isShowing = false;
+  showSubSubMenu: boolean = false;
+
+  mouseenter() {
+    if (!this.isExpanded) {
+      this.isShowing = true;
+    }
+  }
+
+  mouseleave() {
+    if (!this.isExpanded) {
+      this.isShowing = false;
+    }
+  }
+
+
+  constructor(private observer: BreakpointObserver) {
+
+  }
+  
+ 
+  ngOnInit(): void {
+  
+    this.observer.observe(["(max-width: 800px)"])
+    .subscribe((res) => {
+      if(res.matches) {
+        this.sidenav.mode = "over";
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = "side";
+        this.sidenav.open();
+      }
+    })
+  
+  
+  }
+
+
+
+  title = 'meuCrudNew';
+}
